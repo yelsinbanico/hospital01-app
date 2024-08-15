9672047ef04003a46305b942bd980cad5c04d903
Yelsin Bañico <neil.banico.27@unsch.edu.pe>
2024-08-15 10:50:54 -0500

    segundo commit

---
+++ src/main/java/iadataperu/com/demo/Hospital01AppApplication.java
@@ -0,0 +1,13 @@
+package iadataperu.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hospital01AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hospital01AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iadataperu/com/demo/Hospital01AppApplicationTests.java
@@ -0,0 +1,13 @@
+package iadataperu.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hospital01AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
